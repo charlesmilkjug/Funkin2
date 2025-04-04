13279a94fe4725255e6d49ca208db5410e8073f6
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-04-04 15:57:45 -0400

    Revert "Fix certain characters pixel icons not displaying properly in the chart editor."
    This reverts commit 3f20239436ef203c5da265ba31697ac45d8a08df.

--- source/funkin/ui/PixelatedIcon.hx
+++ source/funkin/ui/PixelatedIcon.hx
@@ -28,7 +28,7 @@ class PixelatedIcon extends FlxFilteredSprite
         charPath += "monsterpixel";
       case "mom" | "mom-car":
         charPath += "mommypixel";
-      case "pico-blazin" | "pico-playable" | "pico-speaker" | "pico-pixel" | "pico-holding-nene":
+      case "pico-blazin" | "pico-playable" | "pico-speaker":
         charPath += "picopixel";
       case "gf-christmas" | "gf-car" | "gf-pixel" | "gf-tankmen":
         charPath += "gfpixel";
