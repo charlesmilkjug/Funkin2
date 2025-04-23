d278a8fbedfb22ce822db93a6ddc11bdd3ce6a1b
thesuperpig56 <97651743+thesuperpig56@users.noreply.github.com>
2025-04-22 19:58:00 -0700

    Shortened statement based on suggestion.
    Co-authored-by: Abnormal <86753001+AbnormalPoof@users.noreply.github.com>

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -892,8 +892,8 @@ class PlayState extends MusicBeatSubState
       Countdown.performCountdown();
 
       // Reset the health icons.
-      if (currentStage != null && currentStage.getBoyfriend() != null) currentStage.getBoyfriend().initHealthIcon(false);
-      if (currentStage != null && currentStage.getDad() != null) currentStage.getDad().initHealthIcon(true);
+      currentStage?.getBoyfriend()?.initHealthIcon(false);
+      currentStage?.getDad()?.initHealthIcon(true);
 
       needsReset = false;
     }
