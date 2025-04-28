8aaa966f7a00c8c1fa8bfdc3ca384497f41829a2
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-04-28 19:35:58 -0400

    macro fixins + no more gallery 💔

--- source/funkin/util/macro/SingletonMacro.hx
+++ source/funkin/util/macro/SingletonMacro.hx
@@ -1,18 +1,27 @@
 package funkin.util.macro;
 
 import haxe.macro.Context;
-import haxe.macro.Expr;
-import haxe.macro.Type;
+import haxe.macro.Type.ClassType;
+import haxe.macro.Expr.Field;
+import haxe.macro.Expr.ComplexType;
 
 using haxe.macro.Tools;
 
+/**
+ * A macro which automatically creates a Singleton `instance` property for a class.
+ * Add `implements funkin.util.tools.ISingleton` to your class to use.
+ */
 class SingletonMacro
 {
+  /**
+   * Applies an `instance` static field to the target class.
+   * @return The modified list of fields for the target class.
+   */
   public static macro function build():Array<Field>
   {
-    var cls = Context.getLocalClass().get();
+    var cls:ClassType = Context.getLocalClass().get();
 
-    var fields = Context.getBuildFields();
+    var fields:Array<Field> = Context.getBuildFields();
 
     var clsType:ComplexType = Context.getType('${cls.module}.${cls.name}').toComplexType();
     var newExpr:String = 'new ${cls.module}.${cls.name}()';
