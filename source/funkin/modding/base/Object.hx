bb615b84268f2f0ea7ed7af10289627502d50518
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-05-01 18:29:16 -0400

    more scripted classes???

---
+++ source/funkin/modding/base/Object.hx
@@ -0,0 +1,14 @@
+package funkin.modding.base;
+
+/**
+ * An empty base class meant to be extended by scripts.
+ */
+class Object
+{
+  public function new() {}
+
+  public function toString():String
+  {
+    return "(Object)";
+  }
+}
