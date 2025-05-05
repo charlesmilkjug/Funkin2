333dda093caaf559811dc82ef328363ec143d0ba
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-05-04 21:12:46 -0400

    remove unnecessary tracy frame mark
    alr done by openfl!!!!

--- source/funkin/Preferences.hx
+++ source/funkin/Preferences.hx
@@ -45,7 +45,7 @@ class Preferences
 
   static function get_naughtyness():Bool
   {
-    return Save?.instance?.options?.naughtyness;
+    return Save?.instance?.options?.naughtyness ?? true;
   }
 
   static function set_naughtyness(value:Bool):Bool
@@ -64,7 +64,7 @@ class Preferences
 
   static function get_downscroll():Bool
   {
-    return Save?.instance?.options?.downscroll;
+    return Save?.instance?.options?.downscroll ?? false;
   }
 
   static function set_downscroll(value:Bool):Bool
@@ -102,7 +102,7 @@ class Preferences
 
   static function get_zoomCamera():Bool
   {
-    return Save?.instance?.options?.zoomCamera;
+    return Save?.instance?.options?.zoomCamera ?? false;
   }
 
   static function set_zoomCamera(value:Bool):Bool
@@ -121,15 +121,12 @@ class Preferences
 
   static function get_debugDisplay():Bool
   {
-    return Save?.instance?.options?.debugDisplay;
+    return Save?.instance?.options?.debugDisplay ?? false;
   }
 
   static function set_debugDisplay(value:Bool):Bool
   {
-    if (value != Save.instance.options.debugDisplay)
-    {
-      toggleDebugDisplay(value);
-    }
+    if (value != Save.instance.options.debugDisplay) toggleDebugDisplay(value);
 
     var save = Save.instance;
     save.options.debugDisplay = value;
@@ -228,7 +225,7 @@ class Preferences
 
   static function get_unlockedFramerate():Bool
   {
-    return Save?.instance?.options?.unlockedFramerate;
+    return Save?.instance?.options?.unlockedFramerate ?? false;
   }
 
   static function set_unlockedFramerate(value:Bool):Bool
